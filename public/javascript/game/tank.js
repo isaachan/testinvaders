37fb158428ecd8c835134cefaf00ff5f8dda07d4
Meng Yu <ymeng@thoughtworks.com>
2013-08-13 21:55:48 +0800

    [ymeng] centralize tank

--- public/javascript/game/tank.js
+++ public/javascript/game/tank.js
@@ -1,27 +1,26 @@
 function Tank(bullet) {
-  var width = 66;
-  var height = 42;
-  var initial_y = 500;
+    var width = 66;
+    var height = 42;
+    var initial_y = 500;
+    var initial_x = 450 - width / 2;
 
-  this.team = Team.Earth;
-  this.box = new BoundingBox(0, initial_y, width, height);
-  this.active = true;
-  this.image = "tank";
+    this.team = Team.Earth;
+    this.box = new BoundingBox(initial_x, initial_y, width, height);
+    this.active = true;
+    this.image = "tank";
 
-  this.collide = function() {
-    // TODO: Implement tank collisions or else
-    // the tank is invincible... that might be fun.
-  };
+    this.collide = function () {
+        // TODO: Implement tank collisions or else
+        // the tank is invincible... that might be fun.
+    };
 
-  this.update = function(delta_time, input) {
-    this.box.x = input.mouse.x - (this.box.width / 2);
+    this.update = function (delta_time, input) {
+        if (input.pressed("shoot")) {
+            if (!bullet.active) {
+                bullet.shoot(-200, input.mouse.x, this.box.y);
+            }
+        }
+    };
 
-    if (input.pressed("shoot")) {
-      if (!bullet.active) {
-        bullet.shoot(-200, input.mouse.x, this.box.y);
-      }
-    }
-  };
-
-  return this;
+    return this;
 }
