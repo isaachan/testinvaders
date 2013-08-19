665ce1fa7286db633c089891dd6674565a9a2935
Meng Yu <ymeng@thoughtworks.com>
2013-08-19 10:22:10 +0800

    [ymeng] format code

--- public/javascript/game/invader.js
+++ public/javascript/game/invader.js
@@ -1,70 +1,70 @@
 function Invader(initial_x, initial_y, bullet) {
-  var width = 66;
-  var height = 48;
+    var width = 66;
+    var height = 48;
 
-  this.team = Team.Invaders;
-  this.velocity = 50;
-  this.box = new BoundingBox(initial_x, initial_y, width, height);
-  this.active = true;
-  this.image = "invader";
-  this.hitpoint = 1;
+    this.team = Team.Invaders;
+    this.velocity = 50;
+    this.box = new BoundingBox(initial_x, initial_y, width, height);
+    this.active = true;
+    this.image = "invader";
+    this.hitpoint = 1;
 
-  this.collide = function(other_thing) {
-    if (other_thing.team == Team.Earth) {
-      this.hitpoint --;
-	  
-	  if (this.hitpoint <= 0) {
-	    this.active = false;
-		if (this.image == 'invader') {
-  	  	  window._game.getScore(5);
-	    } else {
-  	      window._game.getScore(20);
-	    }
-	  } else {
-		  this.updateImage();
-	  }
-    }
-  };
-  
-  this.updateImage = function () {
-	  if (this.image == "invader") {
-		  return;
-	  }
-	  
-	  this.image = "invader2_" + this.hitpoint;
-  };
-  
-  this.heteromorphosis = function () {
-	  this.image = "invader2_3";
-	  this.hitpoint = 3;
-	  this.number_of_seconds_between_shots = 20;
-  }
+    this.collide = function (other_thing) {
+        if (other_thing.team == Team.Earth) {
+            this.hitpoint--;
 
-  var number_of_seconds_between_shots = 100;
-  var shoot_countdown = Math.random() * number_of_seconds_between_shots;
-  var its_time_to_shoot = function() {
-    return shoot_countdown <= 0;
-  };
+            if (this.hitpoint <= 0) {
+                this.active = false;
+                if (this.image == 'invader') {
+                    window._game.getScore(5);
+                } else {
+                    window._game.getScore(20);
+                }
+            } else {
+                this.updateImage();
+            }
+        }
+    };
 
-  this.update = function(delta_time) {
-    if(this.active) {
-      shoot_countdown -= delta_time;
+    this.updateImage = function () {
+        if (this.image == "invader") {
+            return;
+        }
 
-      // Shooting
-      if(its_time_to_shoot()) {
-        bullet.shoot(50, this.box.x + (width / 2), this.box.y + (height / 2));
-        shoot_countdown = number_of_seconds_between_shots;
-      }
+        this.image = "invader2_" + this.hitpoint;
+    };
 
-      // Movement
-      this.box.x += delta_time * this.velocity;
-    }
-  };
+    this.heteromorphosis = function () {
+        this.image = "invader2_3";
+        this.hitpoint = 3;
+        this.number_of_seconds_between_shots = 20;
+    };
 
-  this.invade = function() {
-    this.box.y += 10;
-    this.velocity = this.velocity * -1;
-  };
+    var number_of_seconds_between_shots = 100;
+    var shoot_countdown = Math.random() * number_of_seconds_between_shots;
+    var its_time_to_shoot = function () {
+        return shoot_countdown <= 0;
+    };
 
-  return this;
+    this.update = function (delta_time) {
+        if (this.active) {
+            shoot_countdown -= delta_time;
+
+            // Shooting
+            if (its_time_to_shoot()) {
+                bullet.shoot(50, this.box.x + (width / 2), this.box.y + (height / 2));
+                shoot_countdown = number_of_seconds_between_shots;
+            }
+
+            // Movement
+            this.box.x += delta_time * this.velocity;
+        }
+    };
+
+    this.invade = function () {
+        this.box.y += 10;
+        this.velocity = this.velocity * -1;
+    };
+
+    return this;
 }
